d07eedae68837cd2ba3654a82be02b4ea0e71c46
Adrian Lindberg <adrlin@student.chalmers.se>
2016-09-22 15:20:32 +0200

    Added a button to change between the map and list views

--- app/src/main/java/dat255/refugeemap/EventListFragment.java
+++ app/src/main/java/dat255/refugeemap/EventListFragment.java
@@ -1,8 +1,8 @@
 package dat255.refugeemap;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
--- app/src/main/java/dat255/refugeemap/MainActivity.java
+++ app/src/main/java/dat255/refugeemap/MainActivity.java
@@ -1,36 +1,72 @@
 package dat255.refugeemap;
 
 import android.app.FragmentManager;
-import android.support.v4.app.FragmentActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.res.ResourcesCompat;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity
         implements EventListFragment.OnListFragmentInteractionListener
 {
+
+  private ImageButton mButton;
+  private String ACTIVE_FRAGMENT;
+
   @Override protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-    FragmentManager fm = getFragmentManager();
-
-    //replace the default container:
-    fm.beginTransaction().replace(R.id.fragment_container, new GMapFragment()).commit();
+    firstStart();
   }
 
-  //TODO: Denna metod skall kallas när man klickar på Adrians byta-vy knapp för att visa listan
-  public void showEventList(Bundle savedInstanceState){
-//    setContentView(R.layout.activity_dummy);
-//
-//    if (savedInstanceState == null) {
-//      getSupportFragmentManager().beginTransaction().add(R.id.root_layout,
-//              EventListFragment.newInstance()).commit();
-//    }
+  public void showEventList(){
+    if (ACTIVE_FRAGMENT.equals(GMapFragment.class.getSimpleName())) {
+      FragmentManager fm = getFragmentManager();
+      fm.beginTransaction().replace(R.id.fragment_container, new EventListFragment()).commit();
+      ACTIVE_FRAGMENT = EventListFragment.class.getSimpleName();
+    }
+
+    else if (ACTIVE_FRAGMENT.equals(EventListFragment.class.getSimpleName())) {
+      FragmentManager fm = getFragmentManager();
+      fm.beginTransaction().replace(R.id.fragment_container, new GMapFragment()).commit();
+      ACTIVE_FRAGMENT = GMapFragment.class.getSimpleName();
+    }
+  }
+
+  public void firstStart() {
+    FragmentManager fm = getFragmentManager();
+    initializeViews(findViewById(R.id.main_layout));
+    fm.beginTransaction().replace(R.id.fragment_container, new GMapFragment()).commit();
+    ACTIVE_FRAGMENT = GMapFragment.class.getSimpleName();
   }
 
   @Override
   public void onListFragmentInteraction(StaticContent.StaticItem item){
     Toast.makeText(this, "You selected: " + item.title, Toast.LENGTH_SHORT).show();
+  }
+
+  public void initializeViews(View view) {
+    mButton = (ImageButton) view.findViewById(R.id.change_views_button);
+
+    mButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        showEventList();
+        Drawable map = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_map_black_48dp, null);
+        Drawable list = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_list_black_48dp, null);
+        if (mButton != null && map != null && list != null) {
+          if (mButton.getDrawable().getConstantState().equals(map.getConstantState())) {
+            mButton.setImageDrawable(list);
+          }
+          else {
+            mButton.setImageDrawable(map);
+          }
+        }
+      }
+    });
   }
 }
