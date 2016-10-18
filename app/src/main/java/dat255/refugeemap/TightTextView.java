8a3d21fd0c2c78267d42fe8e0aaff05e2cdbe420
Jolo87 <nylundj@student.chalmers.se>
2016-10-18 10:54:52 +0200

    Fixed alignment of title in details view (max. two lines)
    Each line is split at 10em. If the second line is too long, it is interrupted by an ellipsis ('…').

---
+++ app/src/main/java/dat255/refugeemap/TightTextView.java
@@ -0,0 +1,66 @@
+package dat255.refugeemap;
+
+
+import android.content.Context;
+import android.text.Layout;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/*
+ * Courtesy: http://stackoverflow.com/questions/10913384/how-to-make-textview-wrap-its-multiline-content-exactly
+ * Tightly wraps the text when setting the maxWidth.
+ * @author sky
+ */
+
+public class TightTextView extends TextView {
+    private boolean hasMaxWidth;
+
+    public TightTextView(Context context) {
+        this(context, null, 0);
+    }
+
+    public TightTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TightTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        if (hasMaxWidth) {
+            int specModeW = MeasureSpec.getMode(widthMeasureSpec);
+            if (specModeW != MeasureSpec.EXACTLY) {
+                Layout layout = getLayout();
+                int linesCount = layout.getLineCount();
+                if (linesCount > 1) {
+                    float textRealMaxWidth = 0;
+                    for (int n = 0; n < linesCount; ++n) {
+                        textRealMaxWidth = Math.max(textRealMaxWidth, layout.getLineWidth(n));
+                    }
+                    int w = Math.round(textRealMaxWidth);
+                    if (w < getMeasuredWidth()) {
+                        super.onMeasure(MeasureSpec.makeMeasureSpec(w, MeasureSpec.AT_MOST),
+                            heightMeasureSpec);
+                    }
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void setMaxWidth(int maxpixels) {
+        super.setMaxWidth(maxpixels);
+        hasMaxWidth = true;
+    }
+
+    @Override
+    public void setMaxEms(int maxems) {
+        super.setMaxEms(maxems);
+        hasMaxWidth = true;
+    }
+}
